8449c52cff133772b24f882ba7e99cec81c5f7f7
dudashvictor <viktordudash36@gmail.com>
2020-05-15 18:18:49 +0300

    feat: add project section and make slider

--- src/js/main.js
+++ src/js/main.js
@@ -41,10 +41,32 @@ $(document).ready(function () {
     $(function () {
       $('.hero__scroll-down').click(function () {
         $('html, body').animate({
-          scrollTop: 1000
+          scrollTop: 700
         }, 500);
         return false;
       });
     });
   })(jQuery);
+
+  //initialize swiper when document ready
+  var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    slidesPerView: 1
+  });
+
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  next.css('left', prev.width() + 20 + bullets.width() + 20);
+  bullets.css('left', prev.width() + 20);
+
 });
